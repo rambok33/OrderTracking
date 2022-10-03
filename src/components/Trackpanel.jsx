1e09c42a99f10faed424cc4d40f0b5b7b1fff06f
Flicker786 <69352034+Flicker786@users.noreply.github.com>
2022-10-03 18:31:41 +0530

    JavaScpit Code added to track items

--- src/components/Trackpanel.jsx
+++ src/components/Trackpanel.jsx
@@ -3,21 +3,21 @@ import styles from '../style';
 
 const Trackpanel = () => {
   
-// const value = {username};
-// const [formvalue, setform] = useState(value)
-// const handlechange = (e) => {
-//  console.log(e.target);
-//  const {name} =  e.target;
-//  setform({...formvalue,[name]: value})
-// }
+const value = {username};
+const [formvalue, setform] = useState(value)
+const handlechange = (e) => {
+ console.log(e.target);
+ const {name} =  e.target;
+ setform({...formvalue,[name]: value})
+}
 const inputE = (event) => {
  
   setname(event.target.value)
 }
 const [name ,setname] =useState("");
-// const [final, setfinal]= useState();
+
 const onsubmit = () =>{
-  // setfinal(name);
+
   const finalstate = name; 
   console.log(finalstate);
 }
